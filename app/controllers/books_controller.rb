4535723993799084153105ac122e93627af84214
sho-ma <shomaegawa510@gmail.com>
2021-04-10 09:58:59 +0000

    [Finish]課題２

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,6 +1,6 @@
 class BooksController < ApplicationController
   def index
-    @books = Book.all
+    @books= Book.all
     @book = Book.new
   end
 
@@ -12,9 +12,13 @@ class BooksController < ApplicationController
   end
   
   def create
-    book = Book.new(book_params)
-    book.save
-    redirect_to book_path(book.id)
+    @book = Book.new(book_params)
+     if @book.save
+       redirect_to book_path(@book), notice: 'Book was successfully created'
+     else
+       @books = Book.all
+       render :index
+     end
   end
 
   def edit
@@ -22,9 +26,12 @@ class BooksController < ApplicationController
   end
   
   def update
-    book = Book.find(params[:id])
-    book.update(book_params)
-    redirect_to book_path(book.id)
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+      redirect_to book_path(@book), notice: 'Book was successfully update'
+    else
+      render :edit
+    end
   end
   
   
